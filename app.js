c0aa1ebb86f9e2089784f7f4dcc271e7a0d5be55
Ali Ramazani <ramazanim@berea.edu>
2023-06-06 23:04:51 -0400

    moved files to backend

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const express = require("express");
 const https = require("https");
 const request = require("request");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
+
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
+
