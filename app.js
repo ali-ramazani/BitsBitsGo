30e5367303486b868c349b18b457205ce36c41e0
Ali Ramazani <ramazanim@berea.edu>
2023-06-07 10:32:44 -0400

    added feature articles

--- app.js
+++ app.js
@@ -13,8 +13,40 @@ app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res){
+    const name = req.body.name;
+    const email = req.body.email;
+    console.log(name);
+    
+    const data = {
+        members: [
+            { 
+                email_address: email,
+                status: "subscribed",
+                merge_field: {
+                    FNAME: name,
+                }
+            }
+        ]
+    };
+    const jsonData = JSON.stringify(data);
+    const url = "https://us21.api.mailchimp.com/3.0/lists/04ae0072e8";
+    const options = {
+        method: "POST",
+        auth: "ali:692b919f0fe1b100db0433bc9034717b-us21"
+    }
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+});
+
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
 
-
+// 692b919f0fe1b100db0433bc9034717b-us21
+// list id   04ae0072e8
