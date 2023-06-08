246e5ff43d1ffe82e34a1970eb7c07fca99a233a
Ali Ramazani <ramazanim@berea.edu>
2023-06-08 11:04:24 -0400

    added failure/reroute page

--- app.js
+++ app.js
@@ -16,14 +16,13 @@ app.get("/", function(req, res){
 app.post("/", function(req, res){
     const name = req.body.name;
     const email = req.body.email;
-    console.log(name);
     
     const data = {
         members: [
             { 
                 email_address: email,
                 status: "subscribed",
-                merge_field: {
+                merge_fields: {
                     FNAME: name,
                 }
             }
@@ -36,15 +35,25 @@ app.post("/", function(req, res){
         auth: "ali:692b919f0fe1b100db0433bc9034717b-us21"
     }
     const request = https.request(url, options, function(response){
+        if (response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html");
+        } else{
+            res.sendFile(__dirname + "/failure.html");
+        }
+
         response.on("data", function(data){
             console.log(JSON.parse(data));
-        })
-    })
+        });
+    }); 
     request.write(jsonData);
     request.end();
 });
 
-app.listen(3000, function(){
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT|| 3000 , function(){
     console.log("Server is running on port 3000");
 });
 
